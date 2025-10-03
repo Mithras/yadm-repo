89dd78f93495fbf924747432f0d473d7d0814857
Sergei Kizilov <sergeikizilov@meta.com>
2025-10-03 09:30:35 -0700

    migrate to grim and a few setting changes

--- Documents/scripts/swappy-screenshot.sh
+++ Documents/scripts/swappy-screenshot.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-magick "$(cosmic-screenshot --interactive=false -s /tmp --notify=false)" -crop "$(slurp | sed -re 's/([0-9]+),([0-9]+) ([0-9]+)x([0-9]+)/\3X\4+\1+\2/g')" /tmp/magick.png && swappy -f /tmp/magick.png
+# magick "$(cosmic-screenshot --interactive=false -s /tmp --notify=false)" -crop "$(slurp | sed -re 's/([0-9]+),([0-9]+) ([0-9]+)x([0-9]+)/\3X\4+\1+\2/g')" /tmp/magick.png && swappy -f /tmp/magick.png
+grim -g "$(slurp)" - | swappy -f -
