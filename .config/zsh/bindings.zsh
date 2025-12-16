5bade3d30908bf5d84099dd42380732115097996
Sergei Kizilov <sergeikizilov@meta.com>
2025-12-15 20:27:02 -0800

    alacritty and new-line

--- .config/zsh/bindings.zsh
+++ .config/zsh/bindings.zsh
@@ -1,9 +1,7 @@
 typeset -g -A key
 
-# key[Home]="${terminfo[khome]}"
-# key[End]="${terminfo[kend]}"
-key[Home]="^[[H"
-key[End]="^[[F"
+key[Home]="${terminfo[khome]}"
+key[End]="${terminfo[kend]}"
 key[Insert]="${terminfo[kich1]}"
 key[Backspace]="${terminfo[kbs]}"
 key[Delete]="${terminfo[kdch1]}"
@@ -32,10 +30,21 @@ key[Control-Right]="${terminfo[kRIT5]}"
 [[ -n "${key[Control-Left]}"  ]] && bindkey -- "${key[Control-Left]}"  backward-word
 [[ -n "${key[Control-Right]}" ]] && bindkey -- "${key[Control-Right]}" forward-word
 
-# if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
-#   autoload -Uz add-zle-hook-widget
-#   function zle_application_mode_start { echoti smkx }
-#   function zle_application_mode_stop { echoti rmkx }
-#   add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
-#   add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
-# fi
+# Fallback
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+
+# Shift+Enter
+insert-newline() {
+  LBUFFER="${LBUFFER}"$'\n'
+}
+zle -N insert-newline
+bindkey '^J' insert-newline
+
+if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
+  autoload -Uz add-zle-hook-widget
+  function zle_application_mode_start { echoti smkx }
+  function zle_application_mode_stop { echoti rmkx }
+  add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+  add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
